e2de0b60f26849926a0bf5ed2cebc94df49a0526
Muhammad Mubashir <muhammadmubashir2329@gmail.com>
2020-08-31 17:36:25 +0500

    run_analysis.R and README.md completed, tidy_data_summarized.csv added.

--- README.md
+++ README.md
@@ -2,7 +2,32 @@
 Course project to perform cleaning and analysis on data collected from the accelerometers from the Samsung Galaxy S smartphone.
 
 ## To run analysis and tidyup dataset execute the run_analysis.R
-The following procedures will be performed.
+The R code will take care of dependencies as the packages will be installed if not already present.
+The version details is following: 
+*R version 3.6.3 (2020-02-29)
+*dplyr version 1.0.2
+*stringr version 1.4.0
+*Dataset download date (August 30, 2020)
+
+The following functionality will be performed.
 
 ### Download the dataset
 First script will check for dataset and if not in current working directory it will download the dataset form URL http://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip, and then unzip it. It will create UCI HAR Dataset directory.
+
+### Step 1: Merge test and train data 
+First converted data.table to dplyr::tibble as working with dplyr is easy. Then merged xtest and train data to xdata and y train and test data to ydata. Removed the xtestdata, ytestdata, xtraindata and ytraindata to clear space from working environment (RAM).
+
+### Step 2: Extract measurements on Mean and Std
+Read the variable names (name="features.txt" path="./UCI HAR Dataset/features.txt").
+Converted to dplyer::tibble. Then used "stringr" package to search for std and mean in variable names and used filter() to extract only mean and std variables. Then select the columns with filtered variables.
+
+### Step 3: Use descriptiive activity name
+Initially y_test.txt has one variable representing 1 of six states for each row in "x_test.txt". Instead of converting to factor type, add 6 columns with column name is activity name from file "activity_labels.txt" where if activity is true for row then value is 1 else 0.
+
+### Step 4: Descriptive variable names
+Removed the special characters("-,(,)") from variable names. Rename the columns of xdata. Add subject column to xdata.
+
+### Step 5: Create second independent dataset
+First use group_by activity and student and then apply summarize function to calculate mean for each column (for each group).
+
+Write tidy_data_summarized to disk.
--- run_analysis.R
+++ run_analysis.R
@@ -9,5 +9,72 @@ if(!dir.exists("./UCI HAR Dataset")){
 }
 
 
-testdata <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+## Read the x and y train and test data into data.tables
 
+xtestdata <- read.table("./UCI HAR Dataset/test/X_test.txt")
+ytestdata <- read.table("./UCI HAR Dataset/test/y_test.txt")
+xtraindata <- read.table("./UCI HAR Dataset/train/X_train.txt")
+ytraindata <- read.table("./UCI HAR Dataset/train/y_train.txt")
+testsubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+trainsubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+## convert to dplyr tibble
+if("dplyr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("dplyr")
+}
+library(dplyr)
+xtestdata <- tibble::as_tibble(xtestdata)
+ytestdata <- tibble::as_tibble(ytestdata)
+xtraindata <- tibble::as_tibble(xtraindata)
+ytraindata <- tibble::as_tibble(ytraindata)
+trainsubject <- tibble::as_tibble(trainsubject)
+testsubject <- tibble::as_tibble(testsubject)
+
+## merge test and train data
+xdata <- xtraindata %>% add_row(xtestdata)
+ydata <- ytraindata %>% add_row(ytestdata)
+subject <- trainsubject %>% add_row(testsubject)
+
+rm("xtestdata", "xtraindata", "ytestdata", "ytraindata","trainsubject", "testsubject")
+
+## read variable names
+vnames <- read.table("./UCI HAR Dataset/features.txt")
+activitynames <- read.table("./UCI HAR Dataset/activity_labels.txt")
+vnames <- tibble::as_tibble(vnames)
+vnames$V2 <- tolower(vnames$V2) # convert to lower case
+
+
+activitynames <- tibble::as_tibble(activitynames)
+
+## filter out mean and std variables with using stringr package
+if("stringr" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("stringr")
+}
+library(stringr)
+xdata <- xdata%>%select(filter(vnames, str_detect(V2, "mean\\(\\)|std\\(\\)"))$V1)
+
+## Renaming the variables
+vnames$V2 <-sapply(vnames$V2, 
+       function(x){gsub("[^[:alnum:][:blank:]+?&/\\]", "", x)})
+colnames(xdata) <- vnames$V2
+
+
+## using descriptive activity names
+## convert ydata from (int) to represent activity create column with each activity name
+## and assign 1 if true and 0 if false
+xdata <- mutate(xdata,
+      activity = sapply(ydata$V1, function(x){as.character(activitynames$V2[x])}),
+      !!as.character(activitynames$V2[1]) := sapply(ydata$V1, function(x){if (x==1) 1 else 0}),
+      !!as.character(activitynames$V2[2]) := sapply(ydata$V1, function(x){if (x==2) 1 else 0}),
+      !!as.character(activitynames$V2[3]) := sapply(ydata$V1, function(x){if (x==3) 1 else 0}),
+      !!as.character(activitynames$V2[4]) := sapply(ydata$V1, function(x){if (x==4) 1 else 0}),
+      !!as.character(activitynames$V2[5]) := sapply(ydata$V1, function(x){if (x==5) 1 else 0}),
+      !!as.character(activitynames$V2[6]) := sapply(ydata$V1, function(x){if (x==6) 1 else 0}))
+
+xdata <- mutate(xdata, subject = subject, .before = 1)
+
+# write.csv(xdata, "./tidy_data.csv")
+## group by to summarize (create independent data)
+xdata <- group_by(xdata, activity, subject)
+xdata <- summarise(xdata, across(everything(), mean))
+write.csv(xdata, "./tidy_data_summarized.csv")
